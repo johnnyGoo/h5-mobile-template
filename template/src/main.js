bf08cfbcacb8ff45e1b7c04acb85c66a43745c45
Johnny <157781745@qq.com>
2018-01-06 00:34:38 +0800

    update

--- template/src/main.js
+++ template/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import WechatShare form 'johnny-wechat-share'
+import WechatShare from 'johnny-wechat-share'
 Vue.prototype.WECHAT_SHARE= new WechatShare('http://h5.2smart.cn',{debug:false,api:'http://h5.2smart.cn/wechat/js/'});
 new Vue({
   el: '#app',
