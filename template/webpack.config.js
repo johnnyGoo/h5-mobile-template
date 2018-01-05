c8dc9223a6d4f5349bb0c69c141592a4ed7716e5
Johnny <157781745@qq.com>
2018-01-06 03:04:49 +0800

    update app path

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/',
+    publicPath: '',
     filename: 'app_[hash].js'
   },
   module: {
