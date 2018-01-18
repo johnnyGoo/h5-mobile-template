00c6b2e6c7cfbc3070943fa57ccfccd8b81ca2fa
Johnny <157781745@qq.com>
2018-01-18 15:17:16 +0800

    update share

--- template/src/main.js
+++ template/src/main.js
@@ -7,15 +7,17 @@ option.shareData={
   appmessage: {
     title: "",//好友分享标题
     desc: "",//好友分享描述
-    img: "",//好友分享图片
+    img_url: "",//好友分享图片
     link: "" //好友分享链接
   }, timeline: {
     title: "",//朋友圈分享标题
-    img: "",//朋友圈分享图片
+    img_url: "",//朋友圈分享图片
     link: ""//朋友圈分享链接
   }
 }
-Vue.prototype.WECHAT_SHARE= new WechatShare('http://h5.2smart.cn',option);
+
+shareURL = document.location.href;
+window.WECHAT_SHARE = new WechatShare(encodeURIComponent(shareURL), option);
 new Vue({
   el: '#app',
   render: h => h(App)
